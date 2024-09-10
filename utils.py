ae9e492d2c8a64f65e6205fd3a21faf632f43994
divyanshu230792 <divyanshusharma0005@gmail.com>
2024-09-10 21:50:29 +0530

    Revert "rever"
    This reverts commit b1f3f51e876e43a672e56a4922c20b8743f9cdbe.

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pickle
 import json
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import Linear
+from sklearn.linear_model import LinearRegression
 import warnings
 warnings.filterwarnings('ignore')
 import confi
