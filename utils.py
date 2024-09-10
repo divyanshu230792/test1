b1f3f51e876e43a672e56a4922c20b8743f9cdbe
divyanshu230792 <divyanshusharma0005@gmail.com>
2024-09-10 21:42:22 +0530

    rever

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import pickle
 import json
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Linear
 import warnings
 warnings.filterwarnings('ignore')
 import confi
